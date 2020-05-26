4b18dbfbf799b8afc8c6fa15ccada778d55d2d95
Amanda Tree <amandasimonds9@gmail.com>
2020-05-26 14:46:00 -0600

    reinstalled react so react-mdl will work

---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import classes from "./Portfolio.css";
+
+const portfolio = (props) => (
+    <div className={classes.Portfolio}>
+        Portfolio
+    </div>
+);
+
+export default portfolio
