e1cd4001c1e2900b8bafc126e1bf0d133b9f0c51
Amanda Tree <amandasimonds9@gmail.com>
2020-05-26 15:48:13 -0600

    landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,9 +1,43 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+import myPic from "../assets/images/userpicture.jpg"
 
 class LandingPage extends Component {
     render(){
         return(
-            <div><h1>LandingPage Page</h1></div>
+            <div style={{width: "100%", margin: "auto"}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img 
+                        className="myPic"
+                        src={myPic}
+                        alt="myPic"
+                        />
+                        <div className="banner-text">
+                            <h1>Full Stack Web Developer</h1>
+
+                            <hr/>
+                            <p>
+                               Javascript | React | HTML | CSS | Express | MongoDB | MySQL | NodeJS 
+                            </p>
+
+                            
+                            <div className="social-links">
+                                {/* LinkedIn */}
+                                <a href="https://www.linkedin.com/in/amandasimonds/" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true"/>
+                                </a>
+
+                                {/* GitHub */}
+                                <a href="https://www.github.com/amandasimonds/" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github-square" aria-hidden="true"/>
+                                </a>
+
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
