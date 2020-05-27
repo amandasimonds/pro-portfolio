aed2e51b8a612352b49ad1970ca07c393ae32c4f
Amanda Tree <amandasimonds9@gmail.com>
2020-05-27 16:25:59 -0600

    projects page tweaked and links finished

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,127 +1,139 @@
 import React, { Component } from "react";
 
-import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from "react-mdl";
+import { Tabs, Tab, Card, CardTitle, CardText } from "react-mdl";
 import oceanPort from "../assets/images/oceanport.JPG"
 import streamEdpic from "../assets/images/StreamEd.JPG"
 import noteTakerPic from "../assets/images/notetaker.JPG"
 import buggerLoggerPic from "../assets/images/burgerlogger.JPG"
 import hopToIt from "../assets/images/hoptoit.JPG"
 import spacePortPic from "../assets/images/spaceport.JPG"
+import weatherDashPic from "../assets/images/weather.JPG"
 
 class Projects extends Component {
 
     constructor(props) {
         super(props);
-        this.state= { activeTab: 0};
+        this.state = { activeTab: 0 };
     }
 
     toggleCategories() {
         if (this.state.activeTab === 0) {
-            return(
+            return (
                 <div className="projects-grid">
-                {/* // OceanPort */}
-                <Card shadow={5} style={{width: "350px", height: "400px", margin: "auto"}}>
-                    <CardTitle style={{color:"#fff", height: "400px", background: `url(${oceanPort}) center / cover ` }}/>
-                    <CardText>
-                        <span className="project-title">OceanPort</span><br></br>
-                        React Express App built for learning about the ocean through a variety of ways. MySQL used to store data, and Passport for authentication.
+                    {/* // OceanPort */}
+                    <Card shadow={5} style={{ width: "375px", height: "500px", margin: "auto" }}>
+                        <CardTitle style={{ color: "#fff", height: "400px", background: `url(${oceanPort}) center / cover ` }} />
+                            <div className="project-links">
+                            <a href="https://oceanport.herokuapp.com/" rel="noopener noreferrer" target="_blank"><strong>OceanPort</strong></a>  
+                            <a href="https://github.com/amandasimonds/ocean-port" rel="noopener noreferrer" target="_blank"> <i className="fa fa-github-square" aria-hidden="true"/></a>
+                            </div>
+                        <CardText style={{height: "100px", paddingBottom: "20px", paddingTop:"0"}}>
+                        React Express app built to engage users in learning through reading, taking quizzes, and earning badges. User authentication by Passport NPM, Data managed by MySQL. 
                     </CardText>
-                    <CardActions border style={{textAlign: "center"}}>
-                        <Button colored>GitHub</Button>
-                        <Button colored>Deployed Project</Button>
-                    </CardActions>
-                </Card>
 
-                 {/* // StreamEd */}
-                 <Card shadow={5} style={{width: "350px", height: "400px", margin: "auto"}}>
-                 <CardTitle style={{color:"#fff", height: "400px", background: `url(${streamEdpic}) center / cover` }}>
-                 </CardTitle>
-                 <CardText>
-                 <span className="project-title">StreamEd</span><br></br>
-                     React app that streamlines resources for teaching. This app is still in development.
-                 </CardText>
-                 <CardActions border style={{textAlign: "center"}}>
-                     <Button colored>GitHub</Button>
-                     <Button colored>Deployed Project</Button>
-                 </CardActions>
-             </Card>
+                    </Card>
+
+                    {/* // StreamEd */}
+                    <Card shadow={5} style={{ width: "375px", height: "500px", margin: "auto" }}>
+                        <CardTitle style={{ color: "#fff", height: "400px", background: `url(${streamEdpic}) center / cover` }}>
+                        </CardTitle>
+                            <div className="project-links">
+                            <a href="https://amandasimonds.github.io/stream-ed/" rel="noopener noreferrer" target="_blank"><strong>StreamEd</strong></a>  
+                            <a href="https://github.com/amandasimonds/stream-ed" rel="noopener noreferrer" target="_blank"> <i className="fa fa-github-square" aria-hidden="true"/></a>
+                            </div>
+                            <CardText style={{height: "100px", paddingBottom: "20px", paddingTop:"0"}}></CardText>
+                    </Card>
                 </div>
             )
-        } else if (this.state.activeTab === 1){
+        } else if (this.state.activeTab === 1) {
             return (
                 <div className="projects-grid">
- {/* // NoteTaker */}
-                <Card shadow={5} style={{width: "350px", height: "400px", margin: "auto"}}>
-                 <CardTitle style={{color:"#fff", height: "400px", background: `url(${spacePortPic}) left / cover` }}>
-                 </CardTitle>
-                 <CardText>
-                 <span className="project-title">NoteTaker</span><br></br>
-                     Express App for writing and saving notes.
-                 </CardText>
-                 <CardActions border style={{textAlign: "center"}}>
-                     <Button colored>GitHub</Button>
-                     <Button colored>Deployed Project</Button>
-                 </CardActions>
-             </Card>
+                    {/* // NoteTaker */}
+                    <Card shadow={5} style={{ width: "375px", height: "500px", margin: "auto" }}>
+                        <CardTitle style={{ color: "#fff", height: "400px", background: `url(${noteTakerPic}) left / cover` }}>
+                        </CardTitle>
+                        <div className="project-links">
+                            <a href="https://notetaker-amazing.herokuapp.com/" rel="noopener noreferrer" target="_blank"><strong>NoteTaker</strong></a>  
+                            <a href="https://github.com/amandasimonds/note-taker" rel="noopener noreferrer" target="_blank"> <i className="fa fa-github-square" aria-hidden="true"/></a>
+                            </div>
+                            <CardText style={{height: "100px", paddingBottom: "20px", paddingTop:"0"}}></CardText>
+                    </Card>
 
-             {/* // SpacePort */}
-             <Card shadow={5} style={{width: "350px", height: "400px", margin: "auto"}}>
-                 <CardTitle style={{color:"#fff", height: "400px", background: `url(${noteTakerPic}) center / cover` }}>
-                 </CardTitle>
-                 <CardText>
-                 <span className="project-title">SpacePort</span><br></br>
-                     Express App that allows the user to browse books and movies about space.
-                 </CardText>
-                 <CardActions border style={{textAlign: "center"}}>
-                     <Button colored>GitHub</Button>
-                     <Button colored>Deployed Project</Button>
-                 </CardActions>
-             </Card>
+                    {/* // SpacePort */}
+                    <Card shadow={5} style={{ width: "375px", height: "500px", margin: "auto" }}>
+                        <CardTitle style={{ color: "#fff", height: "400px", background: `url(${spacePortPic}) center / cover` }}>
+                        </CardTitle>
+                        <div className="project-links">
+                            <a href="https://spaceport2020.herokuapp.com/" rel="noopener noreferrer" target="_blank"><strong>SpacePort</strong></a>  
+                            <a href="https://github.com/hdcarr87/project-2" rel="noopener noreferrer" target="_blank"> <i className="fa fa-github-square" aria-hidden="true"/></a>
+                            </div>
+                            <CardText style={{height: "100px", paddingBottom: "20px", paddingTop:"0"}}></CardText>
+                    </Card>
                 </div>
             )
-        } else if (this.state.activeTab === 2){
+        } else if (this.state.activeTab === 2) {
             return (
                 <div className="projects-grid">
-                {/* // Burger Logger */}
-                               <Card shadow={5} style={{width: "350px", height: "400px", margin: "auto"}}>
-                                <CardTitle style={{color:"#fff", height: "400px", background: `url(${buggerLoggerPic}) left / cover` }}>
-                                </CardTitle>
-                                <CardText>
-                                <span className="project-title">Burger-Logger</span><br></br>
-                                    JavaScript Handlebars app that simulates burger take out
-                                </CardText>
-                                <CardActions border style={{textAlign: "center"}}>
-                                    <Button colored>GitHub</Button>
-                                    <Button colored>Deployed Project</Button>
-                                </CardActions>
-                            </Card>
+                    {/* // Weather Dashboard */}
+                    <Card shadow={5} style={{ width: "375px", height: "500px", margin: "auto" }}>
+                        <CardTitle style={{ color: "#fff", height: "400px", background: `url(${weatherDashPic}) left / cover` }}>
+                        </CardTitle>
+                        <div className="project-links">
+                            <a href="https://amandasimonds.github.io/WeatherDashboard/" rel="noopener noreferrer" target="_blank"><strong>Weather Dashboard</strong></a>  
+                            <a href="https://github.com/amandasimonds/WeatherDashboard" rel="noopener noreferrer" target="_blank"> <i className="fa fa-github-square" aria-hidden="true"/></a>
+                            </div>
+                            <CardText style={{height: "100px", paddingBottom: "20px", paddingTop:"0"}}></CardText>
+                    </Card>
 
-                            {/* // Hop To It */}
-                            <Card shadow={5} style={{width: "350px", height: "400px", margin: "auto"}}>
-                                <CardTitle style={{color:"#fff", height: "400px", background: `url(${hopToIt}) center / cover` }}>
-                                </CardTitle>
-                                <CardText>
-                                <span className="project-title">Hop To It</span><br></br>
-                                    JavaScript app that allows the user to search a brewery and a random marijuana strain.
-                                </CardText>
-                                <CardActions border style={{textAlign: "center"}}>
-                                    <Button colored>GitHub</Button>
-                                    <Button colored>Deployed Project</Button>
-                                </CardActions>
-                            </Card>
-                               </div>
+                    {/* // Hop To It */}
+                    <Card shadow={5} style={{ width: "375px", height: "500px", margin: "auto" }}>
+                        <CardTitle style={{ color: "#fff", height: "400px", background: `url(${hopToIt}) center / cover` }}>
+                        </CardTitle>
+                        <div className="project-links">
+                            <a href="https://kellyjones-jpg.github.io/Hop-To-It/" rel="noopener noreferrer" target="_blank"><strong>Hop To It</strong></a>  
+                            <a href="https://github.com/kellyjones-jpg/Hop-To-It" rel="noopener noreferrer" target="_blank"> <i className="fa fa-github-square" aria-hidden="true"/></a>
+                            </div>
+                            <CardText style={{height: "100px", paddingBottom: "20px", paddingTop:"0"}}></CardText>
+                    </Card>
+                </div>
+            )
+        } else if (this.state.activeTab === 3) {
+            return (
+                <div className="projects-grid">
+                    {/* // Burger Logger */}
+                    <Card shadow={5} style={{ width: "375px", height: "500px", margin: "auto" }}>
+                        <CardTitle style={{ color: "#fff", height: "400px", background: `url(${buggerLoggerPic}) left / cover` }}>
+                        </CardTitle>
+                        <div className="project-links">
+                            <a href="https://supertastyburgerapp.herokuapp.com/" rel="noopener noreferrer" target="_blank"><strong>Burger Logger</strong></a>  
+                            <a href="https://github.com/amandasimonds/burger-logger" rel="noopener noreferrer" target="_blank"> <i className="fa fa-github-square" aria-hidden="true"/></a>
+                            </div>
+                            <CardText style={{height: "100px", paddingBottom: "20px", paddingTop:"0"}}></CardText>
+                    </Card>
+
+                    {/* OceanPort */}
+                    <Card shadow={5} style={{ width: "375px", height: "500px", margin: "auto" }}>
+                        <CardTitle style={{ color: "#fff", height: "400px", background: `url(${oceanPort}) center / cover ` }} />
+                        <div className="project-links">
+                            <a href="https://oceanport.herokuapp.com/" rel="noopener noreferrer" target="_blank"><strong>OceanPort</strong></a>  
+                            <a href="https://github.com/amandasimonds/ocean-port" rel="noopener noreferrer" target="_blank"> <i className="fa fa-github-square" aria-hidden="true"/></a>
+                            </div>
+                            <CardText style={{height: "100px", paddingBottom: "20px", paddingTop:"0"}}></CardText>
+                    </Card>
+                </div>
             )
         }
     }
-    
 
-    render(){
-        return(
-            <div className="category-tabs">
-                <Tabs activeTab={this.state.activeTab} onChange={(tabId)=> this.setState({ activeTab: tabId})} ripple>
+
+    render() {
+        return (
+            <div>
+                <Tabs className="category-tabs" activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
                     <Tab>React</Tab>
                     <Tab>Express</Tab>
                     <Tab>JavaScript</Tab>
+                    <Tab>MySQL</Tab>
                 </Tabs>
 
                 <div>
