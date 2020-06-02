a655785028e1deaf2ad7a7ed2af35d1db2467148
Amanda Tree <amandasimonds9@gmail.com>
2020-06-02 12:25:11 -0600

    social links on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,31 @@
 import React from "react";
+import TooltipItem from "./UI/Tooltip"
 
 const Footer = (props) => (
-    <div className="header-color">
-        <div className="footer">Built with React | Copyright Amanda Simonds</div>
+    <div>
+        <div className="footer">
+        <div className="social-links">
+                                {/* LinkedIn */}
+                                <a href="https://www.linkedin.com/in/amandasimonds/" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" id="linkedIn" aria-hidden="true"/>
+                                </a>
+                                <TooltipItem tooltipTarget="linkedIn" tooltipText="LinkedIn"/>
+
+                                {/* GitHub */}
+                                <a href="https://www.github.com/amandasimonds/" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github-square" id="github" aria-hidden="true"/>
+                                </a>
+                                <TooltipItem tooltipTarget="github" tooltipText="GitHub"/>
+
+                                {/* Resume */}
+                                <a href="https://drive.google.com/file/d/1S_Cw0BkOG4CtBCsD85G56rGsSrUU9Tu3/view?usp=sharing" rel="noopener noreferrer" target="_blank">
+                                    <i className="far fa-file-pdf" id="resume" aria-hidden="true"/>
+                                </a>
+                                <TooltipItem tooltipTarget="resume" tooltipText="Resume"/>
+
+                            </div>
+        
+        </div>
 
     </div>
 );
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -23,30 +23,7 @@ class LandingPage extends Component {
                             <p>
                                Javascript | React | HTML | CSS | Express | MongoDB | MySQL | NodeJS 
                             </p>
-
-                            
-                            <div className="social-links">
-                                {/* LinkedIn */}
-                                <a href="https://www.linkedin.com/in/amandasimonds/" rel="noopener noreferrer" target="_blank">
-                                    <i className="fa fa-linkedin-square" id="linkedIn" aria-hidden="true"/>
-                                </a>
-                                <TooltipItem tooltipTarget="linkedIn" tooltipText="LinkedIn"/>
-
-                                {/* GitHub */}
-                                <a href="https://www.github.com/amandasimonds/" rel="noopener noreferrer" target="_blank">
-                                    <i className="fa fa-github-square" id="github" aria-hidden="true"/>
-                                </a>
-                                <TooltipItem tooltipTarget="github" tooltipText="GitHub"/>
-
-                                {/* Resume */}
-                                <a href="https://drive.google.com/file/d/1S_Cw0BkOG4CtBCsD85G56rGsSrUU9Tu3/view?usp=sharing" rel="noopener noreferrer" target="_blank">
-                                    <i className="far fa-file-pdf" id="resume" aria-hidden="true"/>
-                                </a>
-                                <TooltipItem tooltipTarget="resume" tooltipText="Resume"/>
-
-                            </div>
                             <br/>
-
                             <h1>About Me</h1>
                             <div className="about-me-container">
                                 <p>Full Stack Web Developer with a background in art education, collaborative and devoted to helping people through web experiences.</p>
